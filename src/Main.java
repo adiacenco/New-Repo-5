c0699034c588dabf77927e15f86b931c6246b837
adiacenco <andrei.diacenco@endava.com>
2020-10-28 15:40:55 +0200

    New Commit T7-4

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-     int a =10;
-     int b=20;
-     double f =12;
+        int a = 10;
+        int b = 20;
+        double f = 12;
+
+        int d =12;
     }
 }
